88af8bfb5f34fa1a9abb9e2b62d76d4287d65aee
MikeB <burton.mj@gmail.com>
2017-03-09 21:42:40 -0700

    finished up to post/occupations; not sure how to get data changes to persist on users object

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,5 +1,6 @@
 var user = require('../user.js');
 
+
 module.exports = {
 
   getName: function (req, res, next){
@@ -9,7 +10,17 @@ module.exports = {
     res.status(200).json({location : user.location})
   },
   getOccupations: function(req, res, next){
-    res.status(200).json({occupations: user.occupations})
+    console.log(req.query);
+    if(req.query){
+      if(req.query.order === 'desc'){
+        var sortedJobs = user.occupations.sort();
+        res.status(200).json({occupations: sortedJobs})
+      } else if(req.query.order === 'asc'){
+        var sortedJobs = user.occupations.reverse();
+        res.status(200).json({occupations: sortedJobs})
+      }
+    }
+     res.status(200).json({occupations: user.occupations})
   },
   getLatestOccupation: function(req, res, next){
     var latestOccupation = user.occupations[user.occupations.length - 1];
@@ -34,6 +45,36 @@ module.exports = {
       res.status(200).json({family: filteredFamily})
     }
     else res.status(200).json({family : user.family})
+  },
+  getRestaurants: function(req, res, next){
+    console.log(req.params)
+    if(req.params){
+      var filteredRestaurant = user.restaurants.filter(function(place){
+        return place.name === req.params.name;
+      })
+      res.status(200).json({restaurants : filteredRestaurant})
+    } else
+    res.status(200).json({restuarants : user.restaurants})
+  },
+
+  putName: function(req, res, next){
+    console.log(req.body.name);
+    user.name = req.body.name;
+    res.send('user name is now ' + user.name);
+  },
+  putLocation: function(req, res, next){
+    console.log(req.body.location);
+    user.location = req.body.location;
+    res.send('user location is now ' + user.location);
+  },
+  addHobbies: function(req, res, next){
+    console.log(req.body.name)
+    user.hobbies.push(
+      {name : req.body.name,
+      type: req.body.type}
+    )
+    console.log(user.hobbies)
+    res.send(user.hobbies);
   }
 
 
--- index.js
+++ index.js
@@ -15,10 +15,23 @@ app.listen(port, function(){
   console.log('now listening on port ' + port)
 })
 
+//GET requests
+
 app.get('/name', mainCtrl.getName);
 app.get('/location', mainCtrl.getLocation);
-app.get('/occupations', mainCtrl.getOccupations);
+app.get('/occupations/', mainCtrl.getOccupations);
 app.get('/occupations/latest', mainCtrl.getLatestOccupation);
 app.get('/hobbies/:type', mainCtrl.getHobbies);
 // app.get('/hobbies/:type', mainCtrl.getHobbies);
 app.get('/family/:gender', mainCtrl.getFamily);
+app.get('/restaurants/:name', mainCtrl.getRestaurants);
+
+//PUT requests
+
+app.put('/name', mainCtrl.putName);
+app.put('/location', mainCtrl.putLocation);
+
+
+//POST requests
+
+app.post('/hobbies', mainCtrl.addHobbies);
